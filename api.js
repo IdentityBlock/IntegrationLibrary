b8f5d82e360cf5b0f791a27a1fc41cb260db556f
KasunIs <91238842+KasunIs@users.noreply.github.com>
2022-09-30 10:20:05 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,50 @@
+const qrgen = require('qrcode');
+
+function request(info, data) {
+    return core(info, data);
+}
+
+function generateQR(token, data) {
+    let qr;
+
+    //data["token"] = token;
+    qrData = '["Token":"' + token + '"' + ',"Information":' + JSON.stringify(data) + ']';
+
+    //console.log(JSON.stringify(data));
+    console.log(qrData);
+
+    qrgen.toString(qrData, {type:'terminal'}, function (err, url) {
+        if (err) {
+            console.log("Error");
+        }
+        qr = url;
+        //console.log(url)
+    });
+
+    //console.log(qr)
+    return qr;
+}
+
+function requestData() {
+
+}
+
+function core(info, data) {
+    let token = generateToken(info);
+    let qr = generateQR(token, data);
+
+    //console.log(qr);
+
+    return qr;
+}
+
+function generateToken(info) {
+    let current = new Date();
+    let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+    let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+    let dateTime = cDate + 't' + cTime;
+
+    return dateTime + info;
+}
+
+console.log(request("abc", ["a", "b"]));
